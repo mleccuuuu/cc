992bf502dadff1424cefbad812c3f21ac4a32b11
mleccuuuu <honglvm1981@gmail.com>
2025-02-27 08:39:11 +0700

    Update menuu.py

--- menuu.py
+++ menuu.py
@@ -200,7 +200,7 @@ in_dong_khung_cau_vong('[TOOL]➩ Nhập Số [1.4] Tool Auto LinkedIn[PC cần 
 in_dong_khung_cau_vong('[TOOL]➩ Nhập Số [1.5] Tool Auto X[PC cần giả lập-Mobile]')
 in_dong_khung_cau_vong('[TOOL]➩ Nhập Số [1.6] Tool Auto Threads[PC cần giả lập-Mobile]')
 in_dong_khung_cau_vong('[TOOL]➩ Nhập Số [1.7] Tool Auto Facebook 100% auto Giải Captcha New Update Thử Nghiệm[PC]')
-in_dong_khung_cau_vong('[TOOL]➩ Nhập Số [1.2] Tool Auto Youtube[Giả lập-Mobile]')
+in_dong_khung_cau_vong('[TOOL]➩ Nhập Số [1.8] Tool Auto Youtube[Giả lập-Mobile]')
 print("\033[1;95m╔\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m╗")
 print("\033[1;95m║  \033[1;32mTool Auto Hustmedia  \033[1;95m║")
 print("\033[1;95m╚\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m═\033[1;36m═\033[1;95m╝")
@@ -276,7 +276,8 @@ elif chon == '7':
 	exec(requests.get('https://raw.githubusercontent.com/mleccuuuu/cc/refs/heads/main/menuhust.py').text)
 elif chon == '2.1':
 	exec(requests.get('https://raw.githubusercontent.com/mleccuuuu/cc/refs/heads/main/tdsunti.py').text)
-
+elif chon == '1.8':
+	exec(requests.get('https://raw.githubusercontent.com/mleccuuuu/cc/refs/heads/main/glytb.py').text)
 
 
 
